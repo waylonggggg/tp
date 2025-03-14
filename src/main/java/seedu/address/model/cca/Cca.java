e3f29afac6f87e97d331d31f3930025006833d4e
Judy1x4 <e1129873@u.nus.edu>
2025-03-14 20:16:26 +0800

    Implement cca class

--- src/main/java/seedu/address/model/cca/Cca.java
+++ src/main/java/seedu/address/model/cca/Cca.java
@@ -1,2 +1,62 @@
-package seedu.address.model.person.cca;public class Cca {
+package seedu.address.model.cca;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents a CCA in the address book.
+ * Guarantees: immutable; name is valid as declared in {@link #isValidCcaName(String)}
+ */
+public class Cca {
+
+    public static final String MESSAGE_CONSTRAINTS = "CCA names should be alphanumeric";
+    public static final String VALIDATION_REGEX = "\\p{Alnum}+";
+
+    public final String ccaName;
+
+    /**
+     * Constructs a {@code Cca}.
+     *
+     * @param ccaName A valid CCA name.
+     */
+    public Cca(String ccaName) {
+        requireNonNull(ccaName);
+        checkArgument(isValidCcaName(ccaName), MESSAGE_CONSTRAINTS);
+        this.ccaName = ccaName;
+    }
+
+    /**
+     * Returns true if a given string is a valid CCA name.
+     */
+    public static boolean isValidCcaName(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Cca)) {
+            return false;
+        }
+
+        Cca otherCca = (Cca) other;
+        return ccaName.equals(otherCca.ccaName);
+    }
+
+    @Override
+    public int hashCode() {
+        return ccaName.hashCode();
+    }
+
+    /**
+     * Format state as text for viewing.
+     */
+    public String toString() {
+        return '[' + ccaName + ']';
+    }
+
 }
