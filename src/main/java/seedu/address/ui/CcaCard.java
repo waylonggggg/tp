a2eaf1b630745b5f72e659aa2ab0937325512ae8
Just to Use R and IDLE <novaji1729@gmail.com>
2025-03-20 20:33:45 +0800

    add list of roles and total numbers of sessions in CCA card

--- src/main/java/seedu/address/ui/CcaCard.java
+++ src/main/java/seedu/address/ui/CcaCard.java
@@ -1,14 +1,13 @@
 package seedu.address.ui;
 
+import java.util.stream.Collectors;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.cca.Cca;
 
-/**
- * An UI component that displays information of a {@code Cca}.
- */
 public class CcaCard extends UiPart<Region> {
 
     private static final String FXML = "CcaListCard.fxml";
@@ -18,9 +17,13 @@ public class CcaCard extends UiPart<Region> {
     @FXML
     private HBox ccaCardPane;
     @FXML
+    private Label id;
+    @FXML
     private Label ccaName;
     @FXML
-    private Label id;
+    private Label roles;
+    @FXML
+    private Label sessionCount;
 
     /**
      * Creates a {@code CcaCard} with the given {@code Cca} and index to display.
@@ -29,11 +32,27 @@ public class CcaCard extends UiPart<Region> {
         super(FXML);
         this.cca = cca;
 
-        // Apply rounded bubble styling
+        // Apply styling for the outer box
         ccaCardPane.getStyleClass().add("cca-box");
 
+        // 1) Numbering: e.g. "1. "
         id.setText(displayedIndex + ". ");
-        ccaName.setText(cca.getCcaName().fullCcaName);
-    }
 
+        // 2) CCA name
+        ccaName.setText(cca.getCcaName().fullCcaName);
+
+        // 3) Show roles (single label, comma-separated)
+        if (cca.getRoles().isEmpty()) {
+            roles.setText("Roles: None");
+        } else {
+            String rolesString = cca.getRoles().stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(", "));
+            roles.setText("Roles: " + rolesString);
+        }
+
+        // 4) Show total sessions
+        sessionCount.setText("Total number of sessions: "
+                + cca.getTotalSessions().getSessionCount());
+    }
 }
