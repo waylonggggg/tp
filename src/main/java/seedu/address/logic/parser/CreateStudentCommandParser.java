22c9d89c8656d2674414bce81bf5c82a326486b2
Waylon <tanwaylon38@gmail.com>
2025-03-27 18:32:03 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/CreateStudentCommandParser.java
+++ src/main/java/seedu/address/logic/parser/CreateStudentCommandParser.java
@@ -5,7 +5,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ROLE;
 
 import java.util.HashSet;
 import java.util.Set;
