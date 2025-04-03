f48bdad3424bfa50ea5b89f5ee0f0be6b75f396b
Waylon <tanwaylon38@gmail.com>
2025-04-03 17:56:41 +0800

    Rename deletecommandparsertest to deletestudentcommandparsertest

--- src/test/java/seedu/address/logic/parser/DeleteStudentCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteStudentCommandParserTest.java
@@ -16,7 +16,7 @@ import seedu.address.logic.commands.DeleteStudentCommand;
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
-public class DeleteCommandParserTest {
+public class DeleteStudentCommandParserTest {
 
     private DeleteStudentCommandParser parser = new DeleteStudentCommandParser();
 
